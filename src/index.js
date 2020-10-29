204787d67844300402eb0ffdd7d07f4ce5ce8c97
GalinovskayaA <galiale@bk.ru>
2020-10-29 21:59:24 +0300

    feat: make function

--- src/index.js
+++ src/index.js
@@ -38,7 +38,24 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
+  let size = 10;
+  let b = [];
+
+  for (let i = 0; i < expr.length/size; i++) {
+    b[i] = expr.slice(i * size, i * size + size);
+  }
+  let c = [];
+  for (let e = 0; e < b.length; e++) {
+    c.push(b[e].replace(/10/g, '.').replace(/00/g, '').replace(/11/g, '-').replace(/\*{10}/g, ' '));
+  }
+  let m = new Map(Object.entries(MORSE_TABLE));
+  let result = [];
+  for (let x = 0; x < c.length; x++) {
+    let j = c[x];
+    let letter = m.get(j)
+    result.push(letter || ' ')
+  }
+  return result.join('')
 }
 
 module.exports = {
